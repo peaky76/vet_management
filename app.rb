ad69c5b2ce98789087e3eb4d70e65e845c26bed4
peaky76 <robertjamespeacock@gmail.com>
2020-09-30 22:41:39 +0100

    if development? added to contrib/all

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require( 'sinatra' )
-require( 'sinatra/contrib/all' )
+require( 'sinatra/contrib/all' ) if development?
 require_relative('./controllers/pets_controller')
 require_relative('./controllers/owners_controller')
 require_relative('./controllers/treatments_controller')
