63c85d0ef8bc8662608f0d32a9ea329eb7a4ada2
peaky76 <robertjamespeacock@gmail.com>
2020-09-30 23:20:11 +0100

    db settings change for heroku deployment

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,9 +4,14 @@ class SqlRunner
 
   def self.run(sql, values = [])
     begin
-      db = PG.connect({dbname: 'vet_surgery', host: 'localhost'})
-      db.prepare("query", sql)
-      result = db.exec_prepared("query", values)
+      # TO RUN LOCALLY
+      # db = PG.connect({dbname: 'vet_surgery', host: 'localhost'})
+      # db.prepare("query", sql)
+      # result = db.exec_prepared("query", values)
+
+      db = PG.connect( {dbname: 'dasdkdsaldkj',
+      host: 'ec2-1-1-1-1-1.compute-1.amazonaws.com',
+      port: 5432, user: 'nsdlkdjalskjd', password: 'sadlskjadlkjASDAD'}
     ensure
       db.close() if db != nil
     end
